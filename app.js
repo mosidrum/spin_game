b0fd27ab37b3d1e6f9edf758f2e26ba283eedc57
mosidrum <mosiokanga@gmail.com>
2022-11-30 01:11:52 +0100

    continuining from where i stopped

--- app.js
+++ app.js
@@ -19,19 +19,23 @@ const btn = makerElement(output, 'button', 'SPIN', 'btn');
 
 // setting up all the game object
 
-const game ={total:3, inPlay:false, coins:100, speed:5,
-totItems:8, main:[]};
+const game = {
+    total: 3, inPlay: false, coins: 100, speed: 5,
+    totItems: 8, main: []
+};
+let spinner = 10;
 
 // listening to the DOM
 window.addEventListener('DOMContentLoaded', init);
 
 
 // clicking the button to spin or stop
-btn.addEventListener('click',(e)=>{
-    if(btn.textContent == 'SPIN' && !game.inPlay){
+btn.addEventListener('click', (e) => {
+    if (btn.textContent == 'SPIN' && !game.inPlay) {
         btn.textContent = 'STOP'
+        spinner = 10;
         startSpin();
-    }else{
+    } else {
         game.inPlay = false;
         cancelAnimationFrame(game.ani);
         btn.textContent = 'SPIN'
@@ -39,25 +43,25 @@ btn.addEventListener('click',(e)=>{
 
 })
 
-function init(){
+function init() {
     console.log('ready');
     gameArea.style.width = game.total * 100 + 'px';
     let leftPos = (document.body.clientWidth - (game.total * 100)) / 2;
     console.log(leftPos);
     gameArea.style.left = leftPos + 'px';
 
-    for(let i = 0; i < game.total; i++){
+    for (let i = 0; i < game.total; i++) {
 
-        game.main[i] = makerElement(gameArea,'div', '', 'wheel')
-        for (let x = 0; x < game.totItems; x++){
+        game.main[i] = makerElement(gameArea, 'div', '', 'wheel')
+        for (let x = 0; x < game.totItems; x++) {
 
-            const el = makerElement(game.main[i],'div', x + 1,'box');
+            const el = makerElement(game.main[i], 'div', x + 1, 'box');
         }
         game.main[i].style.left = i * 100 + 'px';
     }
 }
 
-function makerElement(parent, ele, html, myClass){
+function makerElement(parent, ele, html, myClass) {
     const el = document.createElement(ele);
     el.classList.add(myClass);
     el.innerHTML = html;
@@ -65,14 +69,37 @@ function makerElement(parent, ele, html, myClass){
     return el;
 }
 
-function startSpin(){
+function startSpin() {
     game.inPlay = true;
     console.log('spinning ' + game.inPlay);
-
+    for (let i = 0; i < game.total; i++) {
+        game.main[i].mover = Math.floor(Math.random() * 20);
+    }
     game.ani = requestAnimationFrame(spin);
 }
 
-function spin(){
-    console.log('running');
-    game.ani = requestAnimationFrame(spin);
+function spin() {
+    spinner--;
+    if (spinner <= 0) {
+        game.inPlay = false;
+        cancelAnimationFrame(game.ani);
+        btn.textContent = 'SPIN'
+    }
+    if (game.inPlay) {
+        console.log('running');
+        let holder = [];
+        for (let i = 0; i < game.total; i++) {
+            let el = game.main[i];
+            let elY = el.offsetTop;
+            console.log(el.offsetTop)
+            if (el.mover > 0) {
+                el.mover--;
+                console.log(el.mover);
+                elY += game.speed;
+                el.style.top = elY + 'px';
+            }
+        }
+        game.ani = requestAnimationFrame(spin);
+    }
+
 }
